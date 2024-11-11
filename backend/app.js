04a460c50eb0c6649fd508ac8b1d568e127bce4a
DivyaAnilkumar <divyaanilkumar0410@gmail.com>
2024-11-11 10:36:16 +0530

    message

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const connectDB = require('./db/connection');
 // const authRoutes = require('./routes/adminLoginRoutes');
 const centerRoutes = require('./routes/centerRoutes');
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const tokenRoutes = require('./routes/tokenRoutes');
 const cors = require('cors');
 
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/centers', centerRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/tokens',tokenRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
